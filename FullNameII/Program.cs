f33b6ab7e23ccfacb60fb3ec35fe2701171dbbe5
Temeka <misstemeka1@live.com>
2018-09-05 09:32:18 -0500

    Added comments

--- FullNameII/Program.cs
+++ FullNameII/Program.cs
@@ -7,12 +7,14 @@ namespace FullNameII
     {
         static void Main(string[] args)
         {
+            //This method is asking user to enter in their first & last name to string them
             Console.WriteLine("Please type your first name:");
             string firstName = Console.ReadLine();
 
             Console.WriteLine("Please type your last name:");
             string lastName = Console.ReadLine();
 
+            //Telling system to string names and 'setting up' the next method
             Console.WriteLine(YourFullName(firstName, lastName));
 
             Console.ReadLine();
@@ -20,14 +22,18 @@ namespace FullNameII
 
         private static string YourFullName (string firstName, string lastName)
         {
+            //Initializing StringBuilder
             StringBuilder fullName = new StringBuilder();
 
+            //Adding names to string along with a space inbetween
             fullName.Append(firstName);
             fullName.Append(" ");
             fullName.Append(lastName);
 
+            //prompting to convert stringbuilder to string 
             string fullNameStr = fullName.ToString();
 
+            //intertwining both methods
             return fullNameStr;
 
 
